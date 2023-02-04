6c742b0f12138dd4f6ab3e21fc05a518cd033b6e
jeayoung <k01066624566@gmail.com>
2023-02-04 22:00:00 +0900

    GAuth 인터페이 분리

--- src/main/java/gauth/GAuth.java
+++ src/main/java/gauth/GAuth.java
@@ -1,136 +1,17 @@
 package gauth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import gauth.exception.GAuthException;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.JSONObject;
-
-import java.io.*;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GAuth {
-    private static final ObjectMapper mapper = new ObjectMapper();
-    private static final String GAuthServerURL = "https://server.gauth.co.kr/oauth";
-    private static final String ResourceServerURL = "https://open.gauth.co.kr";
+public interface GAuth {
+    GAuthToken generateToken(String email, String password, String clientId, String clientSecret, String redirectUri) throws IOException;
 
-    private enum Auth{
-        ACCESS,
-        REFRESH
-    }
-    public static GAuthToken generateToken(String email, String password, String clientId, String clientSecret, String redirectUri) throws IOException {
-        String code = generateCode(email, password).getCode();
-        return new GAuthToken(getToken(code, clientId, clientSecret, redirectUri));
-    }
+    GAuthToken generateToken(String code, String clientId, String clientSecret, String redirectUri) throws IOException;
 
-    public static GAuthToken generateToken(String code, String clientId, String clientSecret, String redirectUri) throws IOException {
-        return new GAuthToken(getToken(code, clientId, clientSecret, redirectUri));
-    }
+    GAuthCode generateCode(String email, String password) throws IOException;
 
-    public static GAuthCode generateCode(String email, String password) throws IOException {
-        Map<String, String> body = new HashMap<>();
-        body.put("email", email);
-        body.put("password", password);
-        String code = sendPostGAuthServer(body, null, "/code").get("code");
-        return new GAuthCode(code);
-    }
+    GAuthToken refresh(String refreshToken) throws IOException;
 
-    public static GAuthToken refresh(String refreshToken) throws IOException{
-        if(!refreshToken.startsWith("Bearer "))
-            refreshToken = "Bearer "+refreshToken;
-        return new GAuthToken(sendPatchGAuthServer(null, refreshToken, "/token", Auth.REFRESH));
-    }
-
-    public static GAuthUserInfo getUserInfo(String accessToken) throws IOException{
-        if(!accessToken.startsWith("Bearer "))
-            accessToken = "Bearer "+accessToken;
-        Map<String, Object> map = sendGetResourceServer(accessToken, "/user");
-        return new GAuthUserInfo(map);
-    }
-
-    private static Map<String, String> getToken(String code, String clientId, String clientSecret, String redirectUri) throws IOException {
-        Map<String, String> body = new HashMap<>();
-        body.put("code", code);
-        body.put("clientId", clientId);
-        body.put("clientSecret", clientSecret);
-        body.put("redirectUri", redirectUri);
-        return sendPostGAuthServer(body, null, "/token");
-    }
-
-    private static Map<String, String> sendPostGAuthServer(Map<String, String> body, String token, String url) throws IOException {
-        return sendPost(body, token, GAuthServerURL+url);
-    }
-
-    private static Map<String, String> sendPatchGAuthServer(Map<String, String> body, String token, String url, Auth auth) throws IOException {
-        return sendPatch(body, token, GAuthServerURL+ url, auth);
-    }
-
-    private static Map<String, Object> sendGetResourceServer(String token, String url) throws IOException {
-        return sendGet(token, ResourceServerURL + url);
-    }
-
-    private static Map<String, Object> sendGet(String token, String url) throws IOException {
-        HttpGet request = new HttpGet(url); //GET 메소드 URL 생성
-        request.addHeader("Authorization", token);
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        HttpResponse response = client.execute(request);
-        if(response.getStatusLine().getStatusCode()!=200)
-            throw new RuntimeException();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-        String responseBody = bufferedReader.readLine();
-        bufferedReader.close();
-        return mapper.readValue(responseBody, Map.class);
-    }
-
-    private static Map<String, String> sendPatch(Map<String, String> body, String token, String url, Auth auth) throws IOException {
-        HttpPatch request = new HttpPatch(url);
-        request.setHeader("Accept", "application/json");
-        request.setHeader("Connection", "keep-alive");
-        request.setHeader("Content-Type", "application/json");
-        if(auth == Auth.ACCESS)
-            request.setHeader("Authorization", token);
-        else
-            request.addHeader("refreshToken", token);
-        if(body != null){
-            String json = new JSONObject(body).toJSONString();
-            request.setEntity(new StringEntity(json));
-        }
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        CloseableHttpResponse response = client.execute(request);
-        if(response.getStatusLine().getStatusCode()!=200)
-            throw new RuntimeException();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-        String responseBody = bufferedReader.readLine();
-        bufferedReader.close();
-        return mapper.readValue(responseBody, Map.class);
-    }
-
-    private static Map<String, String> sendPost(Map<String, String> body, String token, String url) throws IOException {
-        HttpPost request = new HttpPost(url);
-        request.setHeader("Accept", "application/json");
-        request.setHeader("Connection", "keep-alive");
-        request.setHeader("Content-Type", "application/json");
-        request.addHeader("Authorization", token);
-        if(body != null){
-            String json = new JSONObject(body).toJSONString();
-            request.setEntity(new StringEntity(json));
-        }
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        CloseableHttpResponse response = client.execute(request);
-        if(response.getStatusLine().getStatusCode()!=200)
-            throw new GAuthException(response.getStatusLine().getStatusCode());
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-        String responseBody = bufferedReader.readLine();
-        bufferedReader.close();
-        return mapper.readValue(responseBody, Map.class);
-    }
-
-
+    GAuthUserInfo getUserInfo(String accessToken) throws IOException;
 }
